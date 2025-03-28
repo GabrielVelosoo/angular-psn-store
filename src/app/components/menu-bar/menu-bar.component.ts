e4665fbcf1802cfc3e7985955dc523132cd155b2
Gabriel Veloso Pinheiro <tr26660es@trf1.gov.br>
2025-03-28 15:34:28 -0300

    develop: Atomizando Components

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MenuBarItemComponent} from './menu-bar-item/menu-bar-item.component';
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [],
+  imports: [
+    MenuBarItemComponent
+  ],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.css'
 })
